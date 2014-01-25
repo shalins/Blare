965c0a370da27ed63db6a6c7360439f7b0f581f6
Spencer Yen <spencersyen@gmail.com>
2014-01-24 23:22:17 -0800

    Initial Commit

---
+++ Blare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blare
+//
+//  Created by Spencer Yen on 1/24/14.
+//  Copyright (c) 2014 Spencer Yen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blare/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Blare
+//
+//  Created by Spencer Yen on 1/24/14.
+//  Copyright (c) 2014 Spencer Yen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
