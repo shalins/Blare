e78bbb0eb00aed71e2bdff41a3bde2e1f0fc517f
Shalin Shah <shalinvs@gmail.com>
2014-01-25 06:15:36 -0800

    New GuestViewController

---
+++ Blare/GuestViewController.h
@@ -0,0 +1,13 @@
+//
+//  GuestViewController.h
+//  Blare
+//
+//  Created by Aakash on 1/25/14.
+//  Copyright (c) 2014 Spencer Yen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuestViewController : UIViewController
+
+@end
---
+++ Blare/GuestViewController.m
@@ -0,0 +1,38 @@
+//
+//  GuestViewController.m
+//  Blare
+//
+//  Created by Aakash on 1/25/14.
+//  Copyright (c) 2014 Spencer Yen. All rights reserved.
+//
+
+#import "GuestViewController.h"
+
+@interface GuestViewController ()
+
+@end
+
+@implementation GuestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
